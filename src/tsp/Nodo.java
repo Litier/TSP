00e97d93d0ffd46ce97209a600c9de207b4d7020
Litier <Litier.4@gmail.com>
2017-05-15 18:42:19 -0400

    Update Nodo.java

--- src/tsp/Nodo.java
+++ src/tsp/Nodo.java
@@ -1,20 +1,21 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package tsp;
 
 import java.awt.Point;
 
 /**
- *
- * @author JavierAros
+ * * Esta clase contiene la representación de las ciudades, guardando la id y su ubicación
  */
 
 public class Nodo {
     private Point posicion;
     private int id;
+     /**
+     * Constructor del nodo
+     * @param id El parámetro id es el identificador del nodo
+     * @param x El parámetro x la posición x dentro del espacio
+     * @param y El parámetro y la posición y dentro del espacio
+     */
     public Nodo(int id, int x, int y){
         this.posicion = new Point(x, y);
         this.id = id;          
